536917913451aed6964f048b26e322022558add6
SanyaKovylin <sanya.kovylin@yandex.ru>
2024-10-01 12:45:48 +0300

    fixed defence

--- Source/main.cpp
+++ Source/main.cpp
@@ -5,12 +5,14 @@ int main(void){
     Stack st = {};
     STACK_ASSERT(st);
     StackCtor(&st, 8);
-    for (int i = 1; i < 113; i++)
+    for (int i = 1; i < 116; i++)
         StackPush(&st, i);
     int e = 12;
     DUMP(st);
-    for  (int i = 0; i < 100; i++)
+    for  (int i = 0; i < 100; i++){
         StackPop(&st, &e);
+        printf("%d\n",e);
+    }
     printf("%d",e);
     return 0;
 }
--- Source/stack.cpp
+++ Source/stack.cpp
@@ -8,6 +8,11 @@
 #include "stackdef.h"
 
 static const int Mask = rand();
+template <typename T>
+inline T val (T value) { return StackCheck(value); }
+#define     val( value )        ( value )
+
+#define verified  || val(src)
 
 #define Max(x, y) (x) > (y) ? (x) : (y)
 
@@ -16,38 +21,45 @@ FILE *flog = fopen("Stack.log", "w");
 StackError StackCtor (Stack *src, size_t length){
 
     src->src = (type *) calloc (1, length * sizeof(type) + 2*Cansize);
-    printf("%p", src->src);
     memcpy(src->src, Can, Cansize);
 
     if (src->src == NULL) return EMPTY_SOURCE;
+
     src->size = length;
     memcpy(((char*) (src->src + length)) + Cansize, Can, Cansize);
 
     src->pointer = 0;
     src->info = {"stack.cpp", Hash(src->src, src->src+src->size), "src"};
-    DUMP(*src);
+
     STACK_ASSERT(*src);
-    return OK;
+    return StackCheck(src);
 }
 
 StackError StackDtor(Stack *src){
     STACK_ASSERT(*src);
+
     free(src->src);
     src->pointer = 0;
     src->size = 0;
+
     STACK_ASSERT(*src);
+    return StackCheck(src);
 }
 
 StackError StackPush (Stack *src, type elem){
     STACK_ASSERT(*src);
+
     if (src->pointer == src->size - 1) {
-        Resize(src, src->size * 2, src->size);
+        Resize(src, src->size * 2, src->size) verified;
     }
+
     ((type*) ((char*) src->src + Cansize))[src->pointer] = elem;
     src->pointer++;
+
     src->info = {"stack.cpp", Hash(src->src, src->src+src->size), "src"};
+
     STACK_ASSERT(*src);
-    return OK;
+    return StackCheck(src);
 }
 
 
@@ -67,20 +79,27 @@ StackError Resize(Stack *src, size_t size, size_t lastsize) {
 
     DUMP(*src);
     STACK_ASSERT(*src);
-    return OK;
+    return StackCheck(src);
 }
 
 
 StackError StackPop (Stack *src, type *elem){
     STACK_ASSERT(*src);
+
     src->pointer--;
-    *elem = src->src[src->pointer + Cansize];
-    src->src[src->pointer + Cansize] = 0;
-    if (src->pointer <= src->size / 4 && src->size > DefaultSize)
-        Resize(src, src->size/2, src->size);
+    *elem = ((type*) ((char*) src->src + Cansize))[src->pointer];
+    ((type*) ((char*) src->src + Cansize))[src->pointer] = 0;
+
     src->info = {"stack.cpp", Hash(src->src, src->src+src->size), "src"};
+
+    if (src->pointer <= src->size / 4 && src->size > DefaultSize){
+        Resize(src, src->size/2, src->size) verified;
+    }
+
+    src->info = {"stack.cpp", Hash(src->src, src->src+src->size), "src"};
+
     STACK_ASSERT(*src);
-    return OK;
+    return StackCheck(src);
 }
 
 StackError StackCheck (Stack *src) {
@@ -118,13 +137,16 @@ StackError StackDump(Stack *src, const char *file, const int line, const char *e
         fprintf(flog, "\n");
     }
     fprintf(flog,"\n--------------------------------------------------------------------------------\n");
-    return OK;
+    return StackCheck(src);
 }
 
 size_t Hash(void* st, void* end) {
+
     assert((char *) end > (char *) st);
+
     unsigned short A = 1;
     unsigned short B = 0;
+
     for (size_t i = 0; i < (size_t) ((char*) end - (char *) st); i++) {
         if (Mask & (1 << i)){
             A += *(((unsigned char *) st) + i);
